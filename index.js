5427a2543a5b429319c1d90c933984676a1a894d
Stefan Müller <stmu@gmx.net>
2015-11-18 23:01:16 +0100

    typo

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var types = require("HAP-NodeJS/accessories/types.js");
+var types = require("hap-nodejs/accessories/types.js");
 var request = require("request");
 var Service, Characteristic, Accessory;;
 
